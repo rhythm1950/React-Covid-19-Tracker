9405872935f4fe86219f82c8f70ec06b16f41b06
rhythm1950 <rhythmshahriar0@gmail.com>
2022-03-25 22:10:20 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Covid from './components/Covid/Covid';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <Header></Header>
       <Covid></Covid>
     </div>
   );
